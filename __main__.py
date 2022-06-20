c1338a361b21f9a3d737417ad4e210d1538a1838
William Mabia <mabiawilliam@gmail.com>
2022-06-20 02:17:56 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
-import passes
+import authentication
 from twitterconnector import *
 
 
 
 def main():
     # Get the latest pass from the passes.py file
-    consumer_key, consumer_secret, access_token, access_secret = passes.get_passes()
+    consumer_key, consumer_secret, access_token, access_secret = authentication.get_passes()
 
     # Create a new TwitterConnector object to connect to twitter API and use custom API functions
     api = TwitterConnector(consumer_key, consumer_secret, access_token, access_secret)
